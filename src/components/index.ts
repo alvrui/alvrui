311c3207e1cbb691d3fd822ac63dfaea3fd9ea38
alvrui <61255408+alvrui@users.noreply.github.com>
2026-08-07 20:57:29 +0200

    Add components index

---
+++ src/components/index.ts
@@ -0,0 +1,20 @@
+// Proyecto
+export { ProyectoSection, ProyectoForm, ProyectoCard } from './Proyecto';
+
+// Personajes
+export { PersonajesSection, PersonajeForm, PersonajeCard } from './Personajes';
+
+// Narrativas
+export { NarrativasSection, NarrativaForm, NarrativaCard } from './Narrativas';
+
+// Tramas
+export { TramasSection, TramaCard, TramaBuilder, StoryElementCard, FilterPanel, SelectedElementsList, StoryElementCatalog } from './Tramas';
+
+// Estructura
+export { EstructuraSection } from './Estructura';
+
+// Agentes
+export { AgentesSection, AgenteCard } from './Agentes';
+
+// Shared
+export { AISuggestionModal, AIButton } from './Shared';
