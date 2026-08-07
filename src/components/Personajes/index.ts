83af94acb406226fd0f8aef24db82ec196c7fd74
alvrui <61255408+alvrui@users.noreply.github.com>
2026-08-07 20:45:43 +0200

    Add Personajes index

---
+++ src/components/Personajes/index.ts
@@ -0,0 +1,3 @@
+export { default as PersonajesSection } from './PersonajesSection';
+export { default as PersonajeForm } from './PersonajeForm';
+export { default as PersonajeCard } from './PersonajeCard';
