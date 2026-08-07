4c5af5eca8a9d871383c72793cb2d8ad32069b2e
alvrui <61255408+alvrui@users.noreply.github.com>
2026-08-07 20:55:24 +0200

    Add Tramas index

---
+++ src/components/Tramas/index.ts
@@ -0,0 +1,7 @@
+export { default as TramasSection } from './TramasSection';
+export { default as TramaCard } from './TramaCard';
+export { default as TramaBuilder } from './TramaBuilder';
+export { default as StoryElementCard } from './StoryElementCard';
+export { default as FilterPanel } from './FilterPanel';
+export { default as SelectedElementsList } from './SelectedElementsList';
+export { default as StoryElementCatalog } from './StoryElementCatalog';
